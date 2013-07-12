be974681d8f9cdf82c7f3e60a6f02a68e5c29413
hubert <hubert77@gmail.com>
2013-07-12 16:22:10 -0700

    Add spec for menu items with multiple items

---
+++ spec/fixtures/multiple_menu_items.rb
@@ -0,0 +1,1 @@
+1,7.0,fernet branca, ginger ale
--- spec/menu_importer.rb
+++ spec/menu_importer.rb
@@ -6,9 +6,13 @@ describe Castlight::MenuImporter do
     it 'returns a collection of RestaurantMenu objects' do
       menu1 = Castlight::RestaurantMenu.new(id: '1', menu_items: [Castlight::MenuItem.new(price: 20.0, items: ['misuji']), Castlight::MenuItem.new(price: 7.0, items: ['fernet branca'])])
       menu2 = Castlight::RestaurantMenu.new(id: '2', menu_items: [Castlight::MenuItem.new(price: 22.0, items: ['zabuton']), Castlight::MenuItem.new(price: 8.0, items: ['fernet branca'])])
-      restaurant_menus = Castlight::MenuImporter.import('spec/fixtures/test_menu.csv')
+      restaurant_menus = Castlight::MenuImporter.import('spec/fixtures/test_menu.csv').sort_by(&:id)
       expect(restaurant_menus.first).to eql(menu1)
       expect(restaurant_menus.last). to eql(menu2)
+    end
+
+    it 'handles multiple menu items' do
+      menu = Castlight::RestaurantMenu.new(id: '1', menu_items: [Castlight::MenuItem.new(price: 7.0, items: ['fernet branca', 'ginger ale'])])
     end
   end
 end
