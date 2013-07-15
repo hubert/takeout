56e954aa1d876243d40d997dd7e5d3cd2ad263a0
hubert <hubert77@gmail.com>
2013-07-14 17:42:58 -0700

    Optimize by pre-filtering unused menu items

--- lib/takeout/restaurant_menu.rb
+++ lib/takeout/restaurant_menu.rb
@@ -12,20 +12,24 @@ module Takeout
       menu_items.sort.eql?(other.menu_items.sort)
     end
 
-    def best_price(order_list, items_on_menu=menu_items)
+    def best_price(order_list)
+      best_price_calculator(order_list, menu_items.select { |item| item.contains_part_of?(order_list) })
+    end
+
+    private
+
+    def best_price_calculator(order_list, items_on_menu)
       return if items_on_menu.empty?
       prices = []
       items_on_menu.each_with_index do |menu_item, index|
-        if menu_item.contains_part_of?(order_list)
-          remaining_items = order_list - menu_item.items
-          prices << if remaining_items.empty?
-            #p "done, price is #{menu_item.price}"
-            menu_item.price
-          else
-            #p "looking for best price of #{remaining_items} in #{items_on_menu.slice(index + 1, items_on_menu.length - 1).map(&:items)}"
-            remaining_best_price = best_price(remaining_items, items_on_menu.slice(index + 1, items_on_menu.length - 1))
-            menu_item.price + remaining_best_price if remaining_best_price
-          end
+        remaining_items = order_list - menu_item.items
+        prices << if remaining_items.empty?
+          #p "done, price is #{menu_item.price}"
+          menu_item.price
+        else
+          #p "looking for best price of #{remaining_items} in #{items_on_menu.slice(index + 1, items_on_menu.length - 1).map(&:items)}"
+          remaining_best_price = best_price_calculator(remaining_items, items_on_menu.slice(index + 1, items_on_menu.length - 1))
+          menu_item.price + remaining_best_price if remaining_best_price
         end
       end    
       prices.compact.min
